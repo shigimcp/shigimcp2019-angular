8674505552bb5cbe28957ce42f1cdaf30b0e3233
shigimcp <shigimcp@hotmail.com>
2019-10-05 12:14:54 -0400

    RE-CREATED GitHub repo - angular.json    - outputPath: changed from "dist/shigimcp2019-angular" to "docs" for GitHub - deleted ChangeContentDirective - replaced favicon.ico - ng build --prod

--- src/app/change-content.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ChangeContentDirective } from './change-content.directive';
-
-describe('ChangeContentDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ChangeContentDirective();
-    expect(directive).toBeTruthy();
-  });
-});
